23972df2db4ebce9b1774109beb91f801b2203ca
summydeveloper <adegbiteadekola54@gmail.com>
2024-04-26 14:39:18 +0100

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import usericon from './assets/images/user icon.png';
 
 function App() {
   return (
-    <main className="text-2xl">
-      Hello Sumiya, Go to App.jsx to edit the page
+    <main className="bg-green">
+      <div className="text-5xl"> Hello Sumaya, Go to App.jsx to edit the page</div>
+    <p className="text-4xl text-green-600 font-medium">hello! just wanted to test  this if it works</p>
+    <img src={usericon} alt="usericon" />
+    <form action="">
+      <label htmlFor="">FirstName:
+      <input className="flex-1 p-2 rounded-1-lg bg-green-300 border-blue-500 border-4 rounded-2xl" type="text" name="fName" placeholder="please type something here"  />
+      <button href='hi.com'>helllo</button>
+      </label>
+    </form>
     </main>
   );
 }
