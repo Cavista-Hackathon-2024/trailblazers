b0af2662a7f60331d9580d09640d9c6d713b4134
LukoOG <emmanueladesipe01@gmail.com>
2024-04-26 14:54:20 +0100

    renamed the openai folder

---
+++ django/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('emergency_response/', views.emergency_response),
+    path('chat_response/', views.chat_response)
+]
---
+++ django/ussd_server/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UssdServerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ussd_server'
