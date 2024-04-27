9f5ce598728fa51f7f5d95236471a1e4d02f9d26
summydeveloper <adegbiteadekola54@gmail.com>
2024-04-27 08:08:32 +0100

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,72 @@ import Selection from "./components/Selection";
 function App() {
   const [chats,setChats] = useState([])
   return (
-    <main className="bg-white-100 py-4 ">
-      <section>
-        <div className="text-2xl text-center font-bold pb-2 border-grey-500 border-b-2">
-          E-Health
+    <div className="left-container">
+      <section className="bg-white-100 py-4 relative flex-[30%]">
+          <div className="text-2xl text-left font-bold pb-2 border-grey-500 border-b-2">
+            <h1>E-health</h1>
+          </div>
+
+          <div className="relative w-full h-[calc(70%)] p-[24px]">
+            <h1>Get Rapid Responses to emergency situations</h1>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+      </section>
+
+      <section className="right-container">
+        <div>
+          <img src='C:/Users/summy/Downloads/Ellipsenews.jpeg' />
         </div>
       </section>
-      {chats.length == 0 ? <Selection chats={chats} setChats={setChats} /> : <ChatScreen chats={chats} /> }
-
-    </main>
+        {chats.length == 0 ? <Selection chats={chats} setChats={setChats} /> : <ChatScreen chats={chats} /> }
+    </div>
+ 
   );
 }
 
--- frontend/src/components/chatscreen.jsx
+++ frontend/src/components/chatscreen.jsx
@@ -1,7 +1,93 @@
+"use client"
+import React,{useState} from "react";
 
-function ChatScreen(chats) {
+export default function ChatScreen() {
+  const [userInput,setUserInput]= useState('');
+  const [chatHistory, setChatHistory]=useState([]);
+  const [isLoading,setIsLoading]=useState(false);
+
+  const handleUserInput= async()=>{
+    setIsLoading(true);
+    let url = 'https://a11e-213-255-128-168.ngrok-free.app/api/chat_response/'//our api endpoint
+    const data = await fetch(
+        url, {
+            'method':'POST',
+            'headers':{
+                'Content-Type':'application/json'
+            },
+            'body': JSON.stringify({message:chatHistory})
+        }
+    )
+
+    let chatCompletion = await data.json()
+    console.log(chatCompletion)
+    setChatHistory((prevChat)=>[
+      ...prevChat,
+      {role: 'user', content: userInput},
+    // ]);
+    // const chatCompletion= await openai.chat.completions.create({
+    //   messages: [...chatHistory, {role: 'assistant', content: userInput}],
+    //   model:'gpt-3.5-turbo'
+    // });
+    ])
+
+    setChatHistory((prevChat)=>[
+      ...prevChat,
+      {role: 'assistant', content: chatCompletion.content}//chatCompletion.choices[0].message.content}
+    ]);
+    setUserInput('');
+    setIsLoading(false);
+  }
     return (
-            <div>hello</div>
-    );
+     <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+        <div className="w-full max-w-screen-md mg-white p-4 rounded-lg shadow-md">
+          <div className="mb-4">
+              <div className="text-4xl font-bold text-blue-800 mb-2">
+                  Chatbot Assistant
+              </div>
+              <p className="text-gray-600 text-lg">
+                  Welcome to the future of AI powered assistant. Ask me anything!!!
+              </p>
+          </div>
+          <div className="mb-4" style={{height: "400px", overflow: 'auto'}}>
+            {chatHistory.map((message,index)=>(
+              <div
+                key ={index}
+                className={`${message.role === 'user' ? 'text-left' : 'text-right'} mb-2`}
+            
+                >
+                  <div className={`message_role ${message.role === 'user' ? 'bg-blue-300 text-blue-800' : 'bg-green-300 text-green-800'}`}>
+                    {message.role === "user" ? 'H':'A'}
+                  </div>
+
+                  <div className={`message ${message.role==='user' ? 'bg-blue-100 text-blue-800' : 'bg-green-100 text-green-800'  } p-2 rounded-md`}>
+                    {message.content}
+                  </div>
+              </div>
+            ))}
+          </div>
+          <form type="submit" className="flex">
+              <input type="text"
+              placeholder="Ask Me Something"
+              value={userInput}
+              onChange={(e)=> setUserInput(e.target.value)}
+              className="flex-1 p-2 rounded-1-lg"
+              />
+              {isLoading ? (
+                <div className="bg-blue-500 text-white p-2 rounded-r-lg animate-pulse">
+                  Loading
+                </div>):(
+                  <button
+                  onClick={handleUserInput}
+                  type='submit'
+                  className= "bg-blue-500 text-white p-2 rounded-r-lg hover:bg-blue-600"
+                  >
+                    Ask
+                  </button>
+              )}
+          </form>
+        </div>
+      </div>
+
+    )
 }
-export default ChatScreen;
